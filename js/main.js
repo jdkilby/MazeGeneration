7b828c7512d980249ff6afe96751092c72cb3e07
JD Kilby <jd@jdkilby.com>
2016-05-27 15:05:12 -0700

    Ported maze generation algorithm to JavaScript

--- js/main.js
+++ js/main.js
@@ -65,30 +65,156 @@ function init() {
 }
 
 function generateMaze()
-{
-	$.get('../scripts/getMaze.php', { width: gridCols, height: gridRows }, processMaze);
-}
+{   
+	var drawObject, i, j, mazeCol, mazeRow;
+	treeSprites = [];
 
-function processMaze(data)
-{
-	// parse string data
-	data = data.substring(2, data.length - 2);
-	data = data.split(/\]\],\[\[/);
-	for ( var i = 0; i < data.length; i ++ ) {
-		data[i] = data[i].split('\],\[');
-		for ( var j = 0; j < data[i].length; j ++ ) {
-			data[i][j] = data[i][j].split(',');
-			for ( var k = 0; k < data[i][j].length; k ++ ) {
-				data[i][j][k] = (data[i][j][k] === "true") || (data[i][j][k] === "[true") || (data[i][j][k] === "true]");
+	// implementation of Eller's Algorithm for 2D, rectangular maze generation
+	// http://www.neocomputer.org/projects/eller.html
+	
+	// create space for maze
+	maze = [];
+	// create space for current row of cells
+	var row = [];
+	// create space to store set id
+	var sets = new Array(gridCols);
+	// store next available id number for set id's
+	var nextSetID = 1;
+	// loop control variable
+	var mazeFinished = false;
+	// other variables that will be used
+	var wallsCreated, currentSet, allowWallCreation, neighborSet;
+
+	// 1. create first row of cells
+	for (i = 0; i < gridCols; i ++ ) {
+		// first cell has a west wall
+		// last cell has an east wall
+		// all cells have a north wall and no south wall
+		row.push([true, i == gridCols - 1, false, i == 0]);
+	}
+
+	while ( !mazeFinished ) {
+		// 2. join any cell not members of a set to a unique set
+		for (i = 0; i < gridCols; i ++ ) {
+			if ( typeof sets[i] === "undefined" ) {
+				sets[i] = nextSetID;
+				nextSetID = nextSetID + 1;
 			}
 		}
+				
+		// 3. create east walls
+		for (i = 0; i < gridCols; i ++ ) {
+			// if not the last cell, decide whether or not to create a wall on the east;
+			// if the cell to the east belongs to the same set, then force it; otherwise, use a 50% chance
+			if ( i < gridCols - 1 ) {
+				if ( sets[i] === sets[i + 1] || Math.random() >= 0.5 ) {
+					row[i][1] = true;
+					// set west wall of cell neighboring to the east
+					row[i + 1][3] = true;
+				}
+				// if not creating wall, cell east of current cell needs to join current cell's set
+				else {
+					sets[i + 1] = sets[i];
+				}
+			}
+		}
+		
+		// 4. create south walls
+		wallsCreated = {};
+		for ( i = 0; i < gridCols; i ++ ) {
+			currentSet = sets[i]
+			// find out if the cell is not the only one in its set or
+			// if it is the only member of its set without a bottom wall;
+			// in either case, the south wall cannot be built
+			allowWallCreation = true
+			if ( sets.filter( function(element) { return element === currentSet } ).length === 1 ) {
+				allowWallCreation = false;
+			}
+			else {
+				// see if this is the last cell of this set
+				if ( sets.slice(i + 1).filter( function(element) { return element === currentSet } ).length === 0 ) {
+					// check to make sure at least one cell in this set has an open wall;
+					// if not, disallow creation of the wall
+					if ( sets.slice(0, i).filter( function(element) { return element === currentSet } ).length === wallsCreated[currentSet] ) {
+						allowWallCreation = false
+					}
+				}
+			}
+
+			// check to see if wallsCreated dictionary needs a new entry
+			if ( typeof wallsCreated[sets[i]] === "undefined" ) {
+				wallsCreated[currentSet] = 0;
+			}
+			// now attempt to create south wall (if allowed, it's a 50% chance)
+			if ( allowWallCreation && Math.random() >= 0.5 ) {
+				row[i][2] = true;
+				// log that a wall has been created for this set
+				if ( typeof wallsCreated[sets[i]] === "number" ) {
+					wallsCreated[currentSet] = wallsCreated[currentSet] + 1;
+				}
+			}
+		}
+			
+		// 5. decide to add rows or complete the maze   
+		if ( maze.length < gridRows - 1 ) {
+			// maze not finished, continue on
+			maze.push(JSON.parse(JSON.stringify(row)));
+			for (i = 0; i < gridCols; i ++ ) {
+				// remove east wall (if not last cell in row)
+				if ( i < gridCols - 1 ) {
+					row[i][1] = false;
+					// remove the east neighbor's west wall
+					row[i + 1][3] = false;
+				}
+				// if south wall exists, remove cell from current set and remove south wall
+				if ( row[i][2] === true ) {
+					delete sets[i]; // IS THIS CORRECT?
+					row[i][2] = false
+					// since south wall exists, north wall of this new cell exists too
+					row[i][0] = true
+				}
+				// if south wall doesn't exist, make sure that this new cell below previous cell
+				// has no north wall
+				else {
+					row[i][0] = false
+				}
+			}
+			// move back to step 2 (beginning of while loop)
+		}
+		else {
+			for (i = 0; i < gridCols; i ++ ) {
+				// add south wall
+				row[i][2] = true
+				// if there is a neighbor to the east and it is part of a different set
+				if ( i < gridCols - 1 ) {
+					currentSet = sets[i]
+					neighborSet = sets[i + 1]
+					if ( currentSet != neighborSet ) {
+						// remove current cell's east wall
+						row[i][1] = false
+						// remove neighbor cell's west wall
+						row[i + 1][3] = false
+						// make every cell part of the neighbor's set part of the current cell's set
+						for (j = 0; j < gridCols; j ++ ) {
+							if ( sets[j] == neighborSet ) {
+								sets[j] = currentSet
+							}
+						}
+					}
+				}
+			}
+			maze.push(JSON.parse(JSON.stringify(row)));
+			mazeFinished = true
+		}
 	}
-	
-	maze = data;
-	
-	treeSprites = [];
-	
-	var drawObject, i, j, mazeCol, mazeRow;
+	// return the maze lists transposed to use the more familiar [col][row] indexing
+	// using clever snippet explained here: http://stackoverflow.com/a/17428705/4747275
+	maze = maze[0].map(function(col, i) { 
+		return maze.map(function(row) { 
+			return row[i] 
+		})
+	});
+
 	for ( i = 0; i < 2 * gridCols + 1; i ++ ) {
 		for ( j = 0; j < 2 * gridRows + 1; j ++ ) {
 			drawObject = false;
@@ -217,7 +343,7 @@ function triggerMazeRegen() {
 // ----------------------------------------------------
 
 function processMouseInput(event)
-{	
+{   
 	if ( settingUpGame ) {
 		return;
 	}
